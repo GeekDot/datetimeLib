311b753b6e4ed071ed42f74008955aaf429fd1c7
zhangyi <zy251021@gmail.com>
2021-02-23 17:42:16 +0800

    fix gitignore

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 
 build:
 	python setup.py sdist bdist_wheel
+
+upload:
+	python setup.py sdist bdist_wheel upload -r https://pypi.21nmc.com/simple/
--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     packages=find_packages(),
     python_requires='>=3.6',
